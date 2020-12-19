4afcc87fbfc73a0613084a5a66eae689a9a49294
Brian Sturgis <sturujisu@gmail.com>
2020-12-18 18:20:06 -0800

    will update and test startup

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -4,12 +4,7 @@ namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
-  [Route("/hello")]
-    public string Hello() { return "Hello friend!"; }
-
-    [Route("/goodbye")]
-    public string Goodbye() { return "ciao"; }
-
+  
     [Route("/")]
     public ActionResult Form()
     {
