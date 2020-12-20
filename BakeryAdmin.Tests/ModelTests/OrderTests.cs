b983d5286594f7278568daee14f3266bafdb349b
Brian Sturgis <sturujisu@gmail.com>
2020-12-20 15:26:51 -0800

    will be a good fail for GetDescription_ReturnsDescription_String

--- BakeryAdmin.Tests/ModelTests/OrderTests.cs
+++ BakeryAdmin.Tests/ModelTests/OrderTests.cs
@@ -19,6 +19,21 @@ namespace BakeryAdmin.Tests
       Order newOrder = new Order("1","2","3","4");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
-    
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "21";
+
+      //Act
+      Order newOrder = new Order("1","2","3","4");
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+
+
   }
 }
