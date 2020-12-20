2c447a3132103349fb78d981110aa9ecfd584ca1
Brian Sturgis <sturujisu@gmail.com>
2020-12-20 15:41:03 -0800

    will be a good pass forGetAll_ReturnsOrders_OrderList test

--- BakeryAdmin.Tests/ModelTests/OrderTests.cs
+++ BakeryAdmin.Tests/ModelTests/OrderTests.cs
@@ -60,13 +60,13 @@ namespace BakeryAdmin.Tests
       // Assert
       CollectionAssert.AreEqual(newOrder, result);
     }
-
+    
     [TestMethod]
     public void GetAll_ReturnsOrders_OrderList()
     {
       //Arrange
       
-      Order newOrder1 = new Order("a","b","c");
+      Order newOrder1 = new Order("a","b","c","d");
       Order newOrder2 = new Order("a","b","c","d");
       List<Order> newOrder = new List<Order> { newOrder1, newOrder2 };
 
