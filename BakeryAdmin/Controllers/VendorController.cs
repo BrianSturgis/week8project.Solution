0778487d42046141ebc33d154bad5782ec352975
Brian Sturgis <sturujisu@gmail.com>
2020-12-20 16:30:32 -0800

    will update VendorController with index method

--- BakeryAdmin/Controllers/VendorController.cs
+++ BakeryAdmin/Controllers/VendorController.cs
@@ -1,3 +1,17 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using BakeryAdmin.Models;
+namespace BakeryAdmin.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    
+  }
+}
